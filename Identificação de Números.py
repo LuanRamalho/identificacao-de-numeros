7c5bd90f17256aa3a87b10e4fd1e1ea041e2666e
LuanRamalho <32373552+LuanRamalho@users.noreply.github.com>
2021-02-18 14:39:26 -0300

    Add files via upload

---
+++ Identificação de Números.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+print("Digite um número inteiro: ")
+num = int(input())
+
+print("")
+print("")
+
+if(num%2==0):
+    print("Número: ",num)
+    print("Este número é divisível por 2.")
+elif(num%3==0):
+    print("Número: ",num)
+    print("Este número é divisível por 3.")
+elif(num%4==0):
+    print("Número: ",num)
+    print("Este número é divisível por 4.")
+elif(num%5==0):
+    print("Número: ",num)
+    print("Este número é divisível por 5.")
+elif(num%6==0):
+    print("Número: ",num)
+    print("Este número é divisível por 6.")
+elif(num%7==0):
+    print("Número: ",num)
+    print("Este número é divisível por 7.")
+elif(num%8==0):
+    print("Número: ",num)
+    print("Este número é divisível por 8.")
+elif(num%9==0):
+    print("Número: ",num)
+    print("Este número é divisível por 9.")
+elif(num%10==0):
+    print("Número: ",num)
+    print("Este número é divisível por 10.")
+
+    
+    
